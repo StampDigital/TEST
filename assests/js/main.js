a86994b9c7aaa5b04055135209219eccd1000bb2
RawOdds <jadphillips@outlook.com>
2016-04-23 08:27:27 +0100

    v 0.5.2

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,19 +1,49 @@
-var app = angular.module('App', []);
-app.controller('suggestedArticles', function($scope) {
-$scope.name_1="TEST";
-$scope.pic_="TEST2";
-$scope.link_1="TEST3"
+// Code goes here
+var app = angular.module("stampDigital", []);
+var MainController = function($scope){
 
-$scope.article_1="In Shalott House";
-$scope.article_1_image="TEST2";
-$scope.article_1_link="TEST3";
-var article_array = [];
-$scope.latest_2="TEST";
-$scope.latest_2_image="TEST2";
-$scope.latest_2_link="TEST3";
+  $scope.message = "Hi hello";
+  $scope.username = "TESTING";
+  $scope.repoSortOrder = "-stargazers_count";
+
+  app.controller("MainController", MainController);
+};
 
 
-$scope.latest_1="In Shalott House";
-$scope.article_1_image="TEST2";
-$scope.article_1_link="TEST3";
-});
+
+
+
+
+
+
+
+
+
+
+
+// var app = angular.module('App', []);
+// app.controller('suggestedArticles', function($scope) {
+// $scope.name_1="TEST";
+// $scope.pic_="TEST2";
+// $scope.link_1="TEST3"
+//
+// $scope.article_1="In Shalott House";
+// $scope.article_1_image="TEST2";
+// $scope.article_1_link="TEST3";
+// var article_array = [];
+// $scope.latest_2="TEST";
+// $scope.latest_2_image="TEST2";
+// $scope.latest_2_link="TEST3";
+//
+//
+// $scope.latest_1="In Shalott House";
+// $scope.article_1_image="TEST2";
+// $scope.article_1_link="TEST3";
+// });
+//
+// var app = angular.module("App", []);
+// var ControllerName = function($scope){
+// $scope.nameOne = "Hello";
+//
+// //app.controller("Articles" );
+// };
