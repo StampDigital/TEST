cbb532535cf745f328987ffcd4d3f51a8b7e80fd
RawOdds <jadphillips@outlook.com>
2016-04-23 08:07:17 +0100

    V0.5

---
+++ Article Content/testingContentStuff.js
@@ -0,0 +1,7 @@
+var articleOne = ;
+var articleOneContent = ; 
+var app = angular.module('myApp', []);
+app.controller('articleContent', function($scope) {
+    $scope.articleOneContent = "BLAH BLAH BLAH BLAH BLAH THING STUFF";
+ 
+});
---
+++ assests/js/main.js
@@ -0,0 +1,33 @@
+var app = angular.module('App', []);
+app.controller('suggestedArticles', function($scope) {
+$scope.name_1="TEST";
+$scope.pic_="TEST2";
+$scope.link_1="TEST3"
+
+$scope.article_1="In Shalott House";
+$scope.article_1_image="TEST2";
+$scope.article_1_link="TEST3";
+var article_array = [];
+$scope.latest_2="TEST";
+$scope.latest_2_image="TEST2";
+$scope.latest_2_link="TEST3";
+
+
+$scope.latest_1="In Shalott House";
+$scope.article_1_image="TEST2";
+$scope.article_1_link="TEST3";
+});
+app.controller('Articles', function($scope) {
+//$scope.article_1="In Shalott House";
+//$scope.article_1_image="TEST2";
+//$scope.article_1_link="TEST3";
+//var article_array = [];
+//$scope.latest_2="TEST";
+//$scope.latest_2_image="TEST2";
+//$scope.latest_2_link="TEST3";
+
+
+//$scope.latest_1="In Shalott House";
+//$scope.article_1_image="TEST2";
+//$scope.article_1_link="TEST3";
+});
